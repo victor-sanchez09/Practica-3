486d116772cef98f63263e72ec05d5344ca11d85
victor-sanchez09 <victor.sanchez02@iest.edu.mx>
2026-03-06 08:29:21 -0600

    Agregamos nuevas materias y mensajes de aprobado o reprobado

--- Practica 3/Practica 3/Form1.Designer.vb
+++ Practica 3/Practica 3/Form1.Designer.vb
@@ -35,6 +35,23 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.resultado = New System.Windows.Forms.Label()
         Me.calcular = New System.Windows.Forms.Button()
+        Me.mat6 = New System.Windows.Forms.Label()
+        Me.mat7 = New System.Windows.Forms.Label()
+        Me.mat8 = New System.Windows.Forms.Label()
+        Me.mat9 = New System.Windows.Forms.Label()
+        Me.mat10 = New System.Windows.Forms.Label()
+        Me.mat11 = New System.Windows.Forms.Label()
+        Me.mat12 = New System.Windows.Forms.Label()
+        Me.mat13 = New System.Windows.Forms.Label()
+        Me.materia6 = New System.Windows.Forms.TextBox()
+        Me.materia7 = New System.Windows.Forms.TextBox()
+        Me.materia8 = New System.Windows.Forms.TextBox()
+        Me.materia9 = New System.Windows.Forms.TextBox()
+        Me.materia10 = New System.Windows.Forms.TextBox()
+        Me.materia11 = New System.Windows.Forms.TextBox()
+        Me.materia12 = New System.Windows.Forms.TextBox()
+        Me.materia13 = New System.Windows.Forms.TextBox()
+        Me.mensaje = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -145,11 +162,167 @@ Partial Class Form1
         Me.calcular.Text = "Calcular promedio"
         Me.calcular.UseVisualStyleBackColor = True
         '
+        'mat6
+        '
+        Me.mat6.AutoSize = True
+        Me.mat6.Location = New System.Drawing.Point(138, 217)
+        Me.mat6.Name = "mat6"
+        Me.mat6.Size = New System.Drawing.Size(44, 13)
+        Me.mat6.TabIndex = 13
+        Me.mat6.Text = "Biologia"
+        '
+        'mat7
+        '
+        Me.mat7.AutoSize = True
+        Me.mat7.Location = New System.Drawing.Point(138, 243)
+        Me.mat7.Name = "mat7"
+        Me.mat7.Size = New System.Drawing.Size(63, 13)
+        Me.mat7.TabIndex = 14
+        Me.mat7.Text = "Paraescolar"
+        '
+        'mat8
+        '
+        Me.mat8.AutoSize = True
+        Me.mat8.Location = New System.Drawing.Point(137, 266)
+        Me.mat8.Name = "mat8"
+        Me.mat8.Size = New System.Drawing.Size(34, 13)
+        Me.mat8.TabIndex = 15
+        Me.mat8.Text = "Fisica"
+        '
+        'mat9
+        '
+        Me.mat9.AutoSize = True
+        Me.mat9.Location = New System.Drawing.Point(137, 291)
+        Me.mat9.Name = "mat9"
+        Me.mat9.Size = New System.Drawing.Size(60, 13)
+        Me.mat9.TabIndex = 16
+        Me.mat9.Text = "Literatura II"
+        '
+        'mat10
+        '
+        Me.mat10.AutoSize = True
+        Me.mat10.Location = New System.Drawing.Point(137, 320)
+        Me.mat10.Name = "mat10"
+        Me.mat10.Size = New System.Drawing.Size(99, 13)
+        Me.mat10.TabIndex = 17
+        Me.mat10.Text = "Formacion Humana"
+        '
+        'mat11
+        '
+        Me.mat11.AutoSize = True
+        Me.mat11.Location = New System.Drawing.Point(137, 351)
+        Me.mat11.Name = "mat11"
+        Me.mat11.Size = New System.Drawing.Size(102, 13)
+        Me.mat11.TabIndex = 18
+        Me.mat11.Text = "Taller mundo laboral"
+        '
+        'mat12
+        '
+        Me.mat12.AutoSize = True
+        Me.mat12.Location = New System.Drawing.Point(91, 386)
+        Me.mat12.Name = "mat12"
+        Me.mat12.Size = New System.Drawing.Size(163, 13)
+        Me.mat12.TabIndex = 19
+        Me.mat12.Text = "Taller comunicacion colaborativa"
+        '
+        'mat13
+        '
+        Me.mat13.AutoSize = True
+        Me.mat13.Location = New System.Drawing.Point(138, 412)
+        Me.mat13.Name = "mat13"
+        Me.mat13.Size = New System.Drawing.Size(85, 13)
+        Me.mat13.TabIndex = 20
+        Me.mat13.Text = "Educacion fisica"
+        '
+        'materia6
+        '
+        Me.materia6.Location = New System.Drawing.Point(271, 214)
+        Me.materia6.Name = "materia6"
+        Me.materia6.Size = New System.Drawing.Size(82, 20)
+        Me.materia6.TabIndex = 21
+        '
+        'materia7
+        '
+        Me.materia7.Location = New System.Drawing.Point(271, 240)
+        Me.materia7.Name = "materia7"
+        Me.materia7.Size = New System.Drawing.Size(82, 20)
+        Me.materia7.TabIndex = 22
+        '
+        'materia8
+        '
+        Me.materia8.Location = New System.Drawing.Point(271, 266)
+        Me.materia8.Name = "materia8"
+        Me.materia8.Size = New System.Drawing.Size(82, 20)
+        Me.materia8.TabIndex = 23
+        '
+        'materia9
+        '
+        Me.materia9.Location = New System.Drawing.Point(271, 291)
+        Me.materia9.Name = "materia9"
+        Me.materia9.Size = New System.Drawing.Size(82, 20)
+        Me.materia9.TabIndex = 24
+        '
+        'materia10
+        '
+        Me.materia10.Location = New System.Drawing.Point(271, 320)
+        Me.materia10.Name = "materia10"
+        Me.materia10.Size = New System.Drawing.Size(82, 20)
+        Me.materia10.TabIndex = 25
+        '
+        'materia11
+        '
+        Me.materia11.Location = New System.Drawing.Point(271, 346)
+        Me.materia11.Name = "materia11"
+        Me.materia11.Size = New System.Drawing.Size(82, 20)
+        Me.materia11.TabIndex = 26
+        '
+        'materia12
+        '
+        Me.materia12.Location = New System.Drawing.Point(271, 379)
+        Me.materia12.Name = "materia12"
+        Me.materia12.Size = New System.Drawing.Size(82, 20)
+        Me.materia12.TabIndex = 27
+        '
+        'materia13
+        '
+        Me.materia13.Location = New System.Drawing.Point(271, 409)
+        Me.materia13.Name = "materia13"
+        Me.materia13.Size = New System.Drawing.Size(82, 20)
+        Me.materia13.TabIndex = 28
+        '
+        'mensaje
+        '
+        Me.mensaje.BackColor = System.Drawing.SystemColors.Control
+        Me.mensaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mensaje.ForeColor = System.Drawing.Color.White
+        Me.mensaje.Location = New System.Drawing.Point(437, 255)
+        Me.mensaje.Name = "mensaje"
+        Me.mensaje.Size = New System.Drawing.Size(285, 93)
+        Me.mensaje.TabIndex = 29
+        Me.mensaje.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 659)
+        Me.Controls.Add(Me.mensaje)
+        Me.Controls.Add(Me.materia13)
+        Me.Controls.Add(Me.materia12)
+        Me.Controls.Add(Me.materia11)
+        Me.Controls.Add(Me.materia10)
+        Me.Controls.Add(Me.materia9)
+        Me.Controls.Add(Me.materia8)
+        Me.Controls.Add(Me.materia7)
+        Me.Controls.Add(Me.materia6)
+        Me.Controls.Add(Me.mat13)
+        Me.Controls.Add(Me.mat12)
+        Me.Controls.Add(Me.mat11)
+        Me.Controls.Add(Me.mat10)
+        Me.Controls.Add(Me.mat9)
+        Me.Controls.Add(Me.mat8)
+        Me.Controls.Add(Me.mat7)
+        Me.Controls.Add(Me.mat6)
         Me.Controls.Add(Me.calcular)
         Me.Controls.Add(Me.resultado)
         Me.Controls.Add(Me.Label6)
@@ -183,4 +356,21 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents resultado As Label
     Friend WithEvents calcular As Button
+    Friend WithEvents mat6 As Label
+    Friend WithEvents mat7 As Label
+    Friend WithEvents mat8 As Label
+    Friend WithEvents mat9 As Label
+    Friend WithEvents mat10 As Label
+    Friend WithEvents mat11 As Label
+    Friend WithEvents mat12 As Label
+    Friend WithEvents mat13 As Label
+    Friend WithEvents materia6 As TextBox
+    Friend WithEvents materia7 As TextBox
+    Friend WithEvents materia8 As TextBox
+    Friend WithEvents materia9 As TextBox
+    Friend WithEvents materia10 As TextBox
+    Friend WithEvents materia11 As TextBox
+    Friend WithEvents materia12 As TextBox
+    Friend WithEvents materia13 As TextBox
+    Friend WithEvents mensaje As Label
 End Class
--- Practica 3/Practica 3/Form1.vb
+++ Practica 3/Practica 3/Form1.vb
@@ -4,7 +4,7 @@
     End Sub
 
     Private Sub calcular_Click(sender As Object, e As EventArgs) Handles calcular.Click
-        Dim mat1, mat2, mat3, mat4, mat5 As Double
+        Dim mat1, mat2, mat3, mat4, mat5, mat6, mat7, mat8, mat9, mat10, mat11, mat12, mat13 As Double
         Dim promedio As Double
 
         mat1 = Val(materia1.Text)
@@ -12,9 +12,30 @@
         mat3 = Val(materia3.Text)
         mat4 = Val(materia4.Text)
         mat5 = Val(materia5.Text)
+        mat6 = Val(materia6.Text)
+        mat7 = Val(materia7.Text)
+        mat8 = Val(materia8.Text)
+        mat9 = Val(materia9.Text)
+        mat10 = Val(materia10.Text)
+        mat11 = Val(materia11.Text)
+        mat12 = Val(materia12.Text)
+        mat13 = Val(materia13.Text)
 
-        promedio = (mat1 + mat2 + mat3 + mat4 + mat5) / 5
+        promedio = (mat1 + mat2 + mat3 + mat4 + mat5 + mat6 + mat7 + mat8 + mat9 + mat10 + mat11 + mat12 + mat13) / 13
 
-        resultado.Text = promedio.ToString()
+        Dim redondeo As Double = Math.Round(promedio, 1)
+
+        resultado.Text = redondeo.ToString()
+
+        If redondeo = 10 Then
+            mensaje.BackColor = Color.Lime
+            mensaje.Text = "¡Excelente!"
+        ElseIf redondeo >= 6 Then
+            mensaje.BackColor = Color.Green
+            mensaje.Text = "¡Aprobado!"
+        Else
+            mensaje.BackColor = Color.Red
+            mensaje.Text = "Reprobado :("
+        End If
     End Sub
 End Class
